642b09cc02258652bfd6a65e8d142f978adacd98
Shahrukh khan <shahrukhkhan78676@gmail.com>
2026-05-10 12:47:03 +0530

    Build update

--- dist/libs/todo-agent/src/lib/agent/memory/radis.service.d.ts
+++
@@ -1,9 +0,0 @@
-import Redis from 'ioredis';
-import { MemoryType } from '../state';
-export declare class RadisMemoryService {
-    private readonly redis;
-    constructor(redis: Redis);
-    setSession(sessionId: string, data: MemoryType[]): Promise<void>;
-    getSession(sessionId: string): Promise<MemoryType[]>;
-    clearSession(sessionId: string): Promise<void>;
-}
--- dist/libs/todo-agent/src/lib/agent/memory/redis.provider.d.ts
+++
@@ -1,5 +0,0 @@
-import Redis from 'ioredis';
-export declare const redisProvider: {
-    provide: string;
-    useFactory: () => Redis;
-};
