d4336fcc409b5f658a6b872a6e7e84ecc62dc571
Shahrukh khan <shahrukhkhan78676@gmail.com>
2026-05-10 12:42:49 +0530

    build change

--- apps/pr-review-agent-runner/webpack.config.js
+++ apps/pr-review-agent-runner/webpack.config.js
@@ -4,7 +4,11 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   output: {
+<<<<<<< HEAD
      path: join(__dirname, '../../dist/apps/pr-review-agent-runner'),
+=======
+    path: join(__dirname, '../../actions/pr-review-agent/dist'),
+>>>>>>> 14c10c1 (build change)
     clean: true,
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
